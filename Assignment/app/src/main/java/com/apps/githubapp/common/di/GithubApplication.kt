562d3979936144ecb8ebd59d7b990720511e7d78
pranjal.singh <pranjal.singh@phonepe.com>
2025-08-05 11:29:45 +0530

    removed irrelevant directory

--- Assignment/app/src/main/java/com/apps/githubapp/common/di/GithubApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.apps.githubapp.common.di
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class GithubApplication: Application()
--- Assignment/app/src/main/java/com/apps/githubapp/common/models/FetchListModel.kt
+++
@@ -1,9 +0,0 @@
-package com.apps.githubapp.common.models
-
-import com.google.gson.annotations.SerializedName
-
-data class FetchListModel(
-    @SerializedName("total_count") val totalCount: Int,
-    @SerializedName("incomplete_results") val incompleteResults: Boolean,
-    @SerializedName("items") val items: List<UserSummary>
-)
